d19865ef0e3327633811cec3e8df9e8c116586c1
TheHegemon <jrwhitman99@gmail.com>
2023-11-18 08:50:03 -0700

    Built the program, should be operational now!

--- pyterm.py
+++ pyterm.py
@@ -1,22 +1,27 @@
+from os.path import join
 import os
+import random
 import sys
 
-def main(verb_only=False, noun_only=False, num_phrases=10):
-    if verb_only and noun_only:
-        print("You selected only verb and only noun output, those conflict.  Turning both off.")
-        verb_only = False
-        noun_only = False
+def main(num_phrases=50):
     # load the relevant files
-    if not verb_only:
-        pass
-    if not noun_only: 
-        pass
+    with open(join('data', 'nouns.txt')) as f:
+        noun_list = [line.strip() for line in f]
+    with open(join('data', 'adjectives.txt')) as f:
+        adj_list = [line.strip() for line in f]
+    with open(join('data', 'verbs.txt')) as f:
+        verb_list = [line.strip() for line in f]
+    with open(join('data', 'adverbs.txt')) as f:
+        adv_list = [line.strip() for line in f]
 
-    print(f"Current path: { __file__}")
-
-    # pick n words at random from the filesets
-
-    # print out a list of 
+    for idx in range(1, num_phrases + 1):
+        noun_or_verb = random.choice(['noun', 'verb'])
+        if noun_or_verb == 'noun':
+            print(f'{idx}: {random.choice(adj_list)} {random.choice(noun_list)}')
+        elif noun_or_verb == 'verb':
+            print(f'{idx}: {random.choice(adv_list)} {random.choice(verb_list)}')
+        else:
+            print('Some error occurred and this did not succeed!  Not sure how it broke though.')
 
 if __name__ == "__main__":
     main()
